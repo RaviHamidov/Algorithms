ad1af40ec85824c453b3940c830b72c12c514b7d
ravihamidov <ravihamidov42@gmail.com>
2022-05-30 12:35:36 +0400

    Fix: Solution for Euclidean Distance Algorithm

--- EuclideanDistance/euclideanDistance.js
+++ EuclideanDistance/euclideanDistance.js
@@ -35,6 +35,6 @@ function euclideanDistance(v1 = [], v2 = []) {
 // (3, 4) və (7, 7) nöqtələri arasındakı məsafə
 console.log(euclideanDistance([3, 4], [7, 7])); // cavab: 5
 // (3, 4) və (4, 3) nöqtələri arasındakı məsafə
-console.log(euclideanDistance([3, 4], [4, 3])); // cavab: 5
+console.log(euclideanDistance([3, 4], [4, 3])); // cavab: 1
 console.log(euclideanDistance([], [])); // cavab: 0
 console.log(euclideanDistance()); // cavab: 0
