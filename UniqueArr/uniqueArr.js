436d2e1cc5154e794e29e204289f3b5a8c4ad9ee
ravihamidov <ravihamidov42@gmail.com>
2022-05-30 12:31:25 +0400

    Fix: Solution for Unique Array Algorithm

--- UniqueArr/uniqueArr.js
+++ UniqueArr/uniqueArr.js
@@ -1,8 +1,6 @@
 /*================================================================
 Solution
 ================================================================*/
-var outputArray = [];
-
 function uniqueArr(arr) {
     let outputArray = arr.filter(function (i, j, crash) {
         return j == crash.indexOf(i);
@@ -10,6 +8,6 @@ function uniqueArr(arr) {
     return outputArray;
 };
 
-console.log(uniqueArr(['a', 'a', 'b', 'c', 'c', 'd', 'b'])); // cavab: ['a', 'b', 'c', 'd']
-console.log(uniqueArr([1, 1, 1, 1, 5])); // cavab: [1, 5]
-console.log(uniqueArr([])); // cavab: []
+console.log(uniqueArr(['a', 'a', 'b', 'c', 'c', 'd', 'b'])); // Answer: ['a', 'b', 'c', 'd']
+console.log(uniqueArr([1, 1, 1, 1, 5])); // Answer: [1, 5]
+console.log(uniqueArr([])); // Answer: []
