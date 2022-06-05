a0110805a356c47291532d499ced8c88b4045331
ravihamidov <ravihamidov42@gmail.com>
2022-06-05 10:15:47 +0400

    Add: Solution with While loop

---
+++ SumFunction/sumWithWhile.js
@@ -0,0 +1,21 @@
+/*================================================================
+Solution_Method-05 with "While" 
+================================================================*/
+function sumMethod_05(arr) {
+    if (arr != undefined) {
+        let i = 0;
+        let sum = 0;
+        while (i < arr.length) {
+            sum += arr[i];
+            i++;
+        }
+        return sum;
+    } else
+        return 0;
+}
+
+console.log(sumMethod_05([1, 2, 3, 4, 5])); // Answer: 15
+console.log(sumMethod_05([-1, -2, -3, -4, -5])); // Answer: -15
+console.log(sumMethod_05([-1, 0, 1, 2, -2])); // Answer: 0
+console.log(sumMethod_05([])); // Answer: 0
+console.log(sumMethod_05()); // Answer: 0
