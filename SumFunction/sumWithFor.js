94b03609b45eae0232213af4ea694c5e7c022b20
ravihamidov <ravihamidov42@gmail.com>
2022-06-05 10:04:46 +0400

    Add: Solution with For loop

---
+++ SumFunction/sumWithFor.js
@@ -0,0 +1,17 @@
+/*================================================================
+Solution_Method-01 with "For"
+================================================================*/
+function sumMethod_01(arr) {
+    if (arr != undefined) {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) sum += arr[i];
+        return sum;
+    } else
+        return 0;
+}
+
+console.log(sumMethod_01([1, 2, 3, 4, 5])); // Answer: 15
+console.log(sumMethod_01([-1, -2, -3, -4, -5])); // Answer: -15
+console.log(sumMethod_01([-1, 0, 1, 2, -2])); // Answer: 0
+console.log(sumMethod_01([])); // Answer: 0
+console.log(sumMethod_01()); // Answer: 0
