d28d312ad6ddba54b76f69db01ab151c114c0eb4
ravihamidov <ravihamidov42@gmail.com>
2022-06-06 10:20:35 +0400

    Add: Solution for randomNumber Algorithms

---
+++ RandomNumber/ranNum.js
@@ -0,0 +1,10 @@
+/*================================================================
+Solution
+================================================================*/
+function randomNumber(lower, upper) {
+    const r = Math.random() * (upper - lower) + lower;
+    return Math.floor(r);
+}
+
+console.log(randomNumber(1, 6)); // Answer: Random Numbers => From 1 to 6 :)
+console.log(randomNumber(0, 10)); // Answer: Random Numbers => From 0 to 10 ;)
