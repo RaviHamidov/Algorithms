24affb7aec2017ab75378642475d997e4dd25a00
ravihamidov <ravihamidov42@gmail.com>
2022-06-09 10:38:20 +0400

    Info: Not Completed

---
+++ UnionOfArray/Readme.md
@@ -0,0 +1,7 @@
+## Algorithm's Instruction
+
+```
+- SumFuntion
+    - Type a function called sum. An array of function numbers
+    i parameter and return the sum of all elements of the array
+```
---
+++ UnionOfArray/unionArr.js
@@ -0,0 +1,32 @@
+/*================================================================
+----------------------------Solution------------------------------
+================================================================*/
+function isValidated(arr) {
+    if (arr != undefined && arr.length > 0 || arr != null) {
+        return true;
+    } else
+        return false;
+}
+
+function unionArr(firstArr, secondaryArr) {
+    if (isValidated(firstArr) && isValidated(secondaryArr)) {
+        for (let i = 0; i < secondaryArr.length; i++) {
+            firstArr.push(secondaryArr[i]);
+        }
+        return firstArr;
+    } else {
+        return [];
+    }
+}
+
+
+console.log(unionArr([20, 12], [8, 15, 6])); // cavab: [20, 12, 8, 15, 6]
+console.log(unionArr([2], [1, 2])); // cavab: [1, 2]
+console.log(unionArr([1, 2, 3])); // cavab: [1, 2, 3]
+console.log(unionArr([], [])); // cavab: []
+console.log(unionArr()); // cavab: []
+
+
+/*================================================================
+--------------------------Not Completed---------------------------
+================================================================*/
