7e30f6810e4e626fdd3d1bf45e47ee901e83afcb
ravihamidov <ravihamidov42@gmail.com>
2022-06-10 15:59:34 +0400

    Add: Solution for Map Algo

---
+++ Map/map.js
@@ -0,0 +1,25 @@
+/*================================================================
+---------------------------Solution-------------------------------
+================================================================*/
+function map(arr, myFunction) {
+    for (let i = 0; i < arr.length; i++) {
+        const result = myFunction(arr[i], i, arr);
+        total.push(result);
+    }
+    return total;
+}
+
+console.log(map([1, 2, 3], function (x) {
+    return x * 5;
+})); // Answer: [2, 4, 6]
+
+console.log(map([3, 6, 9], function (x) {
+    return x * x;
+})); // Answer: [9, 36, 81]
+
+console.log(map([1, 2, 3], function (x) {
+    return [x];
+})); // Answer: [[1], [2], [3]]
+
+console.log(map(['a', 'b', 'c', 'd'], x => null));
+// Answer: [null, null, null, null]
