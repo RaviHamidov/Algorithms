5d4a6fff8645307d56e7ea8ffe016434f86d4892
ravihamidov <ravihamidov42@gmail.com>
2022-06-13 02:45:50 +0400

    Fix: Solution for Geometric Mean Problem

--- GeometricMean/geometricMean.js
+++ GeometricMean/geometricMean.js
@@ -1,6 +1,13 @@
 /*================================================================
-Solution
+-----------------------------Solution-----------------------------
 ================================================================*/
+function isValidated(arr) {
+    if (arr != undefined && arr.length > 0) {
+        return true;
+    } else
+        return false;
+}
+
 function geometricMean(arr) {
     if (isValidated(arr)) {
         let total = 1;
@@ -19,9 +26,6 @@ console.log(geometricMean([15, 12, 13, 19, 10])); // estimated Answer: 13.477
 console.log(geometricMean([])); // Answer: 0
 console.log(geometricMean()); // Answer: 0
 
-function isValidated(arr) {
-    if (arr != undefined && arr.length > 0) {
-        return true;
-    } else
-        return false;
-}
+/*================================================================
+-----------------------Problem Solved ;)--------------------------
+================================================================*/
